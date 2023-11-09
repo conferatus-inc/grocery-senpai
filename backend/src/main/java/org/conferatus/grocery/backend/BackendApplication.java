4b08ccf1e221bd33b659bcf6a484df639df0387b
MihailCCfit <m.tsukanov@g.nsu.ru>
2023-11-09 11:54:57 +0700

    base backend

---
+++ backend/src/main/java/org/conferatus/grocery/backend/BackendApplication.java
@@ -0,0 +1,20 @@
+package org.conferatus.timetable.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableConfigurationProperties
+@EnableScheduling
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/conferatus/timetable/backend/BackendApplicationTests.java
@@ -0,0 +1,12 @@
+package org.conferatus.timetable.backend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BackendApplicationTests {
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
