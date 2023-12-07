3b09f928e91ff98b49c7627399b327225cabde93
MihailCCfit <m.tsukanov@g.nsu.ru>
2023-12-07 08:21:34 +0700

    add headLess

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions
 from fastapi import FastAPI
 import time
 from fastapi.middleware.cors import CORSMiddleware
@@ -18,19 +18,20 @@ def getProducts(raw_qr_code, browser_driver, text_field, button_element):
     button_element.click()
     time.sleep(1.7)
     products = browser_driver.find_elements(By.XPATH, "//div[@class='b-check_place']//tr[@class='b-check_item']")
+    date = browser_driver.find_element(By.XPATH, "//div[@class='b-check_place']//tr[5]/td").text
     product_list = []
     for product in products:
         product_props = product.find_elements(By.XPATH, ".//td")
         product_list.append([y for y in (map(lambda x: x.text, product_props))])
-    return product_list
+    return product_list, date
 
 
 class QrCodeResolver:
     def __init__(self):
         print("creating")
-        self.driver = webdriver.Chrome(
-
-        )
+        options = ChromeOptions()
+        options.add_argument("--headless=new")
+        self.driver = webdriver.Chrome(options=options)
         self.driver.get('https://proverkacheka.com/')
         self.rawQrCodeTab = self.driver.find_element(By.XPATH, "//ul[@class='b-checkform_nav nav nav-tabs']/li[4]")
         self.rawQrCodeTab.click()
@@ -50,25 +51,23 @@ class QrCodeResolver:
 resolver = QrCodeResolver()
 app = FastAPI()
 
-origins = [
-    'http://localhost',
-    'http://localhost:3000',
-]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
 )
 
 """
+http://127.0.0.1:8000/api/qr_code/
 http://127.0.0.1:8000/api/qr_code/t=20201017T1923&s=1498.00&fn=9282440300669857&i=25151&fp=1186123459&n=1
+http://127.0.0.1:8000/api/qr_code/t=20230915T141418&s=160.00&fn=7281440500848943&i=10145&fp=2768303418&n=1
+
 """
 
 
 @app.get('/api/qr_code/{raw_qr}')
 def getQrData(raw_qr: str):
-    time.sleep(10)
     print("qwe", raw_qr)
-    return {'products': getProducts(raw_qr, resolver.driver, resolver.textField, resolver.button)}
+    prods, date = getProducts(raw_qr, resolver.driver, resolver.textField, resolver.button)
+    return {'products': prods, 'date': date}
