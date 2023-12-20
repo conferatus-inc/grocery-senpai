9d3971298de6507512e7a9cb636c39a90296541d
ttrubinov <t.trubinov@g.nsu.ru>
2023-12-20 21:25:15 +0700

    Улучшенное отображение

--- app/src/main/java/inc/conferatus/grocerysenpai/presentation/mainlist/MainListViewModel.kt
+++ app/src/main/java/inc/conferatus/grocerysenpai/presentation/mainlist/MainListViewModel.kt
@@ -1,12 +1,10 @@
 package inc.conferatus.grocerysenpai.presentation.mainlist
 
-import android.annotation.SuppressLint
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import inc.conferatus.grocerysenpai.model.CategoriesListSingleton
 import inc.conferatus.grocerysenpai.model.items.CategoryItem
@@ -14,26 +12,10 @@ import inc.conferatus.grocerysenpai.model.items.GroceryItem
 import inc.conferatus.grocerysenpai.model.repository.GroceryRepository
 import inc.conferatus.grocerysenpai.model.util.CategoriesUtils.Companion.byName
 import inc.conferatus.grocerysenpai.model.util.CategoriesUtils.Companion.filterCategories
-import inc.conferatus.grocerysenpai.model.util.CategoriesUtils.Companion.sortCategories
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.time.LocalDate
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import javax.inject.Inject
@@ -98,8 +80,7 @@ class MainListViewModel @Inject constructor(
 
         viewModelScope.launch {
             suggestedCategories = categoriesListSingleton.categories
-                .filterCategories(inputBeginning = textInput)
-                .sortCategories(limit = 7, input = textInput)
+                .filterCategories(inputBeginning = textInput, limit = 10)
                 .map { it.name }
         }
     }
