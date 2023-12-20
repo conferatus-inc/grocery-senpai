f6a02df4407269b8fa622beb1e4e5d5336303dde
ttrubinov <t.trubinov@g.nsu.ru>
2023-12-20 21:11:33 +0700

    Damerau–Levenshtein distance

--- app/src/main/java/inc/conferatus/grocerysenpai/model/util/CategoriesUtils.kt
+++ app/src/main/java/inc/conferatus/grocerysenpai/model/util/CategoriesUtils.kt
@@ -9,11 +9,13 @@ class CategoriesUtils {
             inputBeginning: String,
             limit: Int = this.size
         ): List<CategoryItem> {
-            return this.filter {
-                it.name.startsWith(inputBeginning, ignoreCase = true)
-                        || it.name.contains(inputBeginning, ignoreCase = true)
-            }
-                .take(limit)
+//            return this.filter {
+//                it.name.startsWith(inputBeginning, ignoreCase = true)
+//                        || it.name.contains(inputBeginning, ignoreCase = true)
+//            }
+            return this.sortedWith { string1, string2 ->
+                compareStringWithInput(string1.name, string2.name, inputBeginning)
+            }.take(limit)
         }
 
         // todo сортировка по частоте использования + нахождению в списке
@@ -23,9 +25,10 @@ class CategoriesUtils {
             // comparator??
             // something else?
         ): List<CategoryItem> {
-            return this.sortedWith { string1, string2 ->
-                compareStringWithInput(string1.name, string2.name, input)
-            }.take(limit)
+//            return this.sortedWith { string1, string2 ->
+//                compareStringWithInput(string1.name, string2.name, input)
+//            }.take(limit)
+            return this.take(limit)
         }
 
         fun List<CategoryItem>.byName(
@@ -35,19 +38,53 @@ class CategoriesUtils {
         }
 
         private fun compareStringWithInput(string1: String, string2: String, input: String): Int {
-            if (string1 == string2) {
-                return 0
+            return compareValues(
+                optimalStringAlignmentDistance(string1, input),
+                optimalStringAlignmentDistance(string2, input)
+            )
+//            return 0
+//            if (string1 == string2) {
+//                return 0
+//            }
+//            if (string1.startsWith(input, ignoreCase = true)) {
+//                if (string2.startsWith(input, ignoreCase = true)) {
+//                    return compareValues(string1.length, string2.length)
+//                }
+//                return -1
+//            }
+//            if (string2.startsWith(input, ignoreCase = true)) {
+//                return 1
+//            }
+//            return compareValues(string1.length, string2.length)
+        }
+
+        fun optimalStringAlignmentDistance(s1: String, s2: String): Int {
+            // Create a table to store the results of subproblems
+//            val dp = Array(s1.length + 1) IntArray (s2.length + 1)
+            val dp = Array(s1.length + 1) { IntArray(s2.length + 1) }
+
+            // Initialize the table
+            for (i in 0..s1.length) {
+                dp[i][0] = i
             }
-            if (string1.startsWith(input, ignoreCase = true)) {
-                if (string2.startsWith(input, ignoreCase = true)) {
-                    return compareValues(string1.length, string2.length)
+            for (j in 0..s2.length) {
+                dp[0][j] = j
+            }
+
+            // Populate the table using dynamic programming
+            for (i in 1..s1.length) {
+                for (j in 1..s2.length) {
+                    if (s1[i - 1] == s2[j - 1]) {
+                        dp[i][j] = dp[i - 1][j - 1]
+                    } else {
+                        dp[i][j] =
+                            1 + minOf(dp[i - 1][j], minOf(dp[i][j - 1], dp[i - 1][j - 1]))
+                    }
                 }
-                return -1
             }
-            if (string2.startsWith(input, ignoreCase = true)) {
-                return 1
-            }
-            return compareValues(string1.length, string2.length)
+
+            // Return the edit distance
+            return dp[s1.length][s2.length]
         }
     }
 }
