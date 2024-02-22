a740bc898ebaaf2399c0ece37cb6580d30cf9ad9
ttrubinov <t.trubinov@g.nsu.ru>
2024-02-22 11:20:09 +0700

    main backend

---
+++ mainbackend/src/main/kotlin/org/example/mainbackend/MainbackendApplication.kt
@@ -0,0 +1,11 @@
+package org.example.mainbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MainbackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MainbackendApplication>(*args)
+}
---
+++ mainbackend/src/test/kotlin/org/example/mainbackend/MainbackendApplicationTests.kt
@@ -0,0 +1,12 @@
+package org.example.mainbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainbackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
