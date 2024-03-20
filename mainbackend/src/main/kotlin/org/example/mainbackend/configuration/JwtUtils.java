583ecd8081f8f24919777871910ef9cafa01eba3
ttrubinov <t.trubinov@g.nsu.ru>
2024-03-20 22:36:20 +0700

    JwtUtils fix

--- mainbackend/src/main/kotlin/org/example/mainbackend/configuration/JwtUtils.java
+++ mainbackend/src/main/kotlin/org/example/mainbackend/configuration/JwtUtils.java
@@ -4,15 +4,17 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.RequiredArgsConstructor;
 import org.example.mainbackend.model.User;
+import org.example.mainbackend.service.AccountService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.yandex.shbr.ost.backend.security.appUser.AccountService;
 
 import java.util.Date;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class JwtUtils {
     private final long accessTokenLifetime = 5*60*1000;
     private final long refreshTokenLifetime = 500*60*1000;
