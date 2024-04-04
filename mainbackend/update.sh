31d2d0993503785ae56ee1ebce6542d879d3e1d7
MihailCCfit <m.tsukanov@g.nsu.ru>
2024-04-04 14:10:15 +0700

    deploy docker container

--- mainbackend/update.sh
+++ mainbackend/update.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cd /tmp
-ls -al
-sudo docker stop grocery_container || true
-sudo docker load -i grocery_backend.tar
-sudo docker run --rm -d -p 8080:8080 --name grocery_container grocery_backend:latest
+cd /tmp;
+ls -al;
+sudo docker ps;
+sudo docker stop grocery_container || true;
+sudo docker load -i grocery_backend.tar;
+sudo docker run --rm -d -p 8080:8080 --name grocery_container grocery_backend:latest;
