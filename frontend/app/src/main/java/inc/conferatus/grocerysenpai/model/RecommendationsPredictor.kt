2d5b28f948af46b549263874916e40b15d73d721
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2024-05-02 11:10:08 +0700

    Revert "predictor по трем переменным (не работает)"
    This reverts commit 942aee35d9b7d4eed370966142261d84cc24eb0e.

--- frontend/app/src/main/java/inc/conferatus/grocerysenpai/model/RecommendationsPredictor.kt
+++ frontend/app/src/main/java/inc/conferatus/grocerysenpai/model/RecommendationsPredictor.kt
@@ -1,50 +1,29 @@
 package inc.conferatus.grocerysenpai.model
 
-import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression
+import org.apache.commons.math3.stat.regression.SimpleRegression
+import kotlin.math.max
+
+const val THRESHOLD = 0.9
 
 fun predict(dayNumbers: List<Long>): Long {
-//    if (dayNumbers.size < 3) {
-//        return -1;
-//    }
-
-    val THRESHOLD = 0.8
-
-    fun convert(dayNumber: Long): DoubleArray {
-        return doubleArrayOf(dayNumber.toDouble(), (dayNumber / 7).toDouble(), (dayNumber / 30).toDouble())
-    }
-
-    fun predictedResult(regression: OLSMultipleLinearRegression, triple: DoubleArray): Double {
-        return regression.estimateRegressionParameters()[0] * triple[0] +
-                regression.estimateRegressionParameters()[1] * triple[1] +
-                regression.estimateRegressionParameters()[2] * triple[2]
-    }
-
-
-    val regression = OLSMultipleLinearRegression()
-
-    val x = mutableListOf<DoubleArray>()
-    val y = mutableListOf<Double>()
+    val regression = SimpleRegression()
 
     var iterDay: Long = 0
     var currBuys: Long = 0
 
     for (dayNo in dayNumbers.distinct().sorted()) {
         while (iterDay < dayNo) {
-            x.add(convert(iterDay))
-            y.add(currBuys.toDouble())
+            regression.addData(iterDay.toDouble(), currBuys.toDouble())
             iterDay++
         }
 
         currBuys++
         iterDay++
-        x.add(convert(dayNo))
-        y.add(currBuys.toDouble())
+        regression.addData(dayNo.toDouble(), currBuys.toDouble())
     }
 
-    regression.newSampleData(y.toDoubleArray(), x.toTypedArray())
-
     var nextDay = iterDay + 1
-    while (predictedResult(regression, convert(nextDay)) < currBuys + THRESHOLD) {
+    while (regression.predict(nextDay.toDouble()) < currBuys + THRESHOLD) {
         nextDay++
     }
 
