1e0ef311d98e1f26738943762b853c7e7ae9fcf0
trubinovta <t.trubinov@g.nsu.ru>
2024-05-15 22:31:50 +0700

    actions

--- mainbackend/python/update_python_qr.sh
+++ mainbackend/python/update_python_qr.sh
@@ -5,4 +5,7 @@ ls -al;
 sudo docker ps;
 sudo docker stop grocery_container_python || true;
 sudo docker load -i grocery_python_qr.tar;
+sudo docker ps -a
 sudo docker run --rm -d -p 8084:8084 --name grocery_container_python grocery_python_qr:latest;
+echo DONE
+sudo docker ps -a
--- mainbackend/update.sh
+++ mainbackend/update.sh
@@ -5,4 +5,7 @@ ls -al;
 sudo docker ps;
 sudo docker stop grocery_container || true;
 sudo docker load -i grocery_backend.tar;
+sudo docker ps -a
 sudo docker run --rm -d -p 8080:8080 --name grocery_container grocery_backend:latest;
+echo DONE
+sudo docker ps -a
