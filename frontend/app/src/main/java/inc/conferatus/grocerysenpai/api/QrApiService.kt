0458133e31c8e3537be22f9c336b7891d458fe93
trubinovta <t.trubinov@g.nsu.ru>
2024-05-23 01:47:54 +0700

    fix qr api

--- frontend/app/src/main/java/inc/conferatus/grocerysenpai/api/QrApiService.kt
+++ frontend/app/src/main/java/inc/conferatus/grocerysenpai/api/QrApiService.kt
@@ -6,9 +6,7 @@ import retrofit2.http.Path
 
 interface QrApiService {
     @GET("qr_code/{raw_qr}")
-    @Deprecated(message = "not implemented yet")
     suspend fun getQrData(
-        @Header("Authorization") token: String,
         @Path("raw_qr") qrData: String,
     ): QrProductsDto
 }
